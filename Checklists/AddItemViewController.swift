4b2262372acade79ffbe1ec8b334b2ac3a889947
rhkwj <rhkwj@naver.com>
2018-08-08 15:34:07 +0800

    page 262

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class AddItemViewControllerTableViewController: UITableViewController {
+class AddItemViewController: UITableViewController {
 
+@IBAction func cancel() {
+    navigationController?.popViewController(animated: true)
+    }
+    
+@IBAction func done() {
+    navigationController?.popViewController(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
