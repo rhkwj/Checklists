15f6c06718dd4f80884269172bba9c83300d8e7b
rhkwj <rhkwj@naver.com>
2018-08-16 15:28:16 +0800

    page 377

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -16,22 +16,8 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         // Enable large titles
         navigationController?.navigationBar.prefersLargeTitles = true
         // Add placeholder data
+         loadChecklists()
         
-        var item = ChecklistItem()
-        item.text = "Item for Birthdays"
-        lists[0].items.append(item)
-        
-        item = ChecklistItem()
-        item.text = "Item for Groceries"
-        lists[1].items.append(item)
-        
-        item = ChecklistItem()
-        item.text = "Item for Cool Apps"
-        lists[2].items.append(item)
-        
-        item = ChecklistItem()
-        item.text = "Item for To Do"
-        lists[3].items.append(item)
         
         // Add placeholder item data
         for list in lists {
@@ -129,4 +115,39 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         }
         navigationController?.popViewController(animated: true)
     }
+
+func documentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    
+     return paths[0]
+    
+    }
+func dataFilePath() -> URL {
+    return documentsDirectory().appendingPathComponent( "Checklists.plist")
+    }
+    
+// this method is now called saveChecklists()
+func saveChecklists() {
+    let encoder = PropertyListEncoder()
+    do {
+        // You encode lists instead of "items"
+        let data = try encoder.encode(lists)
+        try data.write(to: dataFilePath(),
+                       options: Data.WritingOptions.atomic)
+        print("Error encoding item array!")
+    } catch { }
+}
+// this method is now called loadChecklists()
+func loadChecklists() {
+    let path = dataFilePath()
+    if let data = try? Data(contentsOf: path) {
+        let decoder = PropertyListDecoder()
+        do {
+            // You decode to an object of [Checklist] type to lists
+            lists = try decoder.decode([Checklist].self, from: data)
+        } catch {
+            print("Error decoding item array!")
+        }
+    }
+}
 }
