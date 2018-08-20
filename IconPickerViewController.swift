624eeceabe4b8735d8cd7a2b8cc78f2d1e3d95ab
rhkwj <rhkwj@naver.com>
2018-08-20 10:14:23 +0800

    page 424

--- IconPickerViewController.swift
+++ IconPickerViewController.swift
@@ -6,30 +6,38 @@
 //  Copyright © 2018 Kim Yeon Jeong. All rights reserved.
 //
 
-import Foundation
 import UIKit
+
 protocol IconPickerViewControllerDelegate: class {
-    func iconPicker(_ picker: IconPickerViewController,didPick iconName: String)
+    func iconPicker(_ picker: IconPickerViewController,
+                    didPick iconName: String)
 }
+
 class IconPickerViewController: UITableViewController {
     weak var delegate: IconPickerViewControllerDelegate?
-    let icons = [ "No Icon", "Appointments", "Birthdays", "Chores",
-                  "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips" ]
-}
-
-// MARK:- Table View Delegates
-override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return icons.count
-}
-
-override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let icons = [ "No Icon", "Appointments", "Birthdays", "Chores", "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips" ]
     
-    let cell = tableView.dequeueReusableCell(withIdentifier: "IconCell", for: indexPath)
+    // MARK:- Table View Delegates
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return icons.count
+    }
     
-    let iconName = icons[indexPath.row]
-    cell.textLabel!.text = iconName
-    cell.imageView!.image = UIImage(named: iconName)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IconCell", for: indexPath)
+        
+        let iconName = icons[indexPath.row]
+        cell.textLabel!.text = iconName
+        cell.imageView!.image = UIImage(named: iconName)
+        
+        return cell
+    }
     
-    return cell
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath) {
+        if let delegate = delegate {
+            let iconName = icons[indexPath.row]
+            delegate.iconPicker(self, didPick: iconName)
+        }
+    }
 }
-
