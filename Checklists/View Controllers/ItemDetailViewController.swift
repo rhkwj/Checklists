6ff758937196f05ddf4f95b7842a1712bc4d50d6
rhkwj <rhkwj@naver.com>
2018-08-23 10:40:34 +0800

    page 459

--- Checklists/View Controllers/ItemDetailViewController.swift
+++ Checklists/View Controllers/ItemDetailViewController.swift
@@ -78,6 +78,44 @@ class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
         }
     }
     
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell {
+            if indexPath.section == 1 && indexPath.row == 2 {
+                return datePickerCell
+            } else {
+                return super.tableView(tableView, cellForRowAt: indexPath)
+            }
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        if section == 1 && datePickerVisible {
+            return 3
+        } else {
+            return super.tableView(tableView,
+                                   numberOfRowsInSection: section)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 1 && indexPath.row == 2 {
+            return 217
+        } else {
+            return super.tableView(tableView, heightForRowAt: indexPath)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        textField.resignFirstResponder()
+        if indexPath.section == 1 && indexPath.row == 1 {
+            showDatePicker()
+        }
+    }
+    
     // MARK:- Prviate Methods
     func updateDueDateLabel() {
         let formatter = DateFormatter()
